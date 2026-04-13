d00273bae161f5b148e8f1c551b2b6aa9c3767ee
Rodrigo Jiménez <jimenez.rodrigo.dev@gmail.com>
2026-04-13 16:33:09 -0400

    hotfix: added warning for solver

--- cmd/neural-sync-engine-launcher/solve_size.go
+++ cmd/neural-sync-engine-launcher/solve_size.go
@@ -9,8 +9,9 @@ import (
 
 var sizeSolverCmd = &cobra.Command{
 	Use:   "solve",
-	Short: "Run the NeuralSyncTester config solver auxiliary tool",
-	Long:  `Creates valid configurations for a given size to use in NeuralSyncTester engine.`,
+	Short: "(EXPERIMENTAL) Run the NeuralSyncTester config solver auxiliary tool",
+	Long: `Creates valid configurations for a given size to use in NeuralSyncTester engine. 
+	Experimental. To use, you must recompile with you own settings.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// solverOutput := config_solver.SolveSize_NoOverlap(1, 3, 0, 3)
 		solverOutput := config_solver.SolveSize_Overlap(2, 9, 1, 3, "PARTIAL OVERLAP")
