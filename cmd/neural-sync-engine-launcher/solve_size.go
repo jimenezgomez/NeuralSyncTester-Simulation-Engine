54b61575e4b49fb1169621766840921d44d2455b
Rodrigo Jiménez <jimenez.rodrigo.dev@gmail.com>
2026-04-13 16:30:19 -0400

    fix: fixed gitignore ignoring launcher folder

---
+++ cmd/neural-sync-engine-launcher/solve_size.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+
+	config_solver "github.com/jimenezgomez/NeuralSyncTester-Simulation-Engine/internal/config_manager/solve"
+	"github.com/spf13/cobra"
+)
+
+var sizeSolverCmd = &cobra.Command{
+	Use:   "solve",
+	Short: "Run the NeuralSyncTester config solver auxiliary tool",
+	Long:  `Creates valid configurations for a given size to use in NeuralSyncTester engine.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// solverOutput := config_solver.SolveSize_NoOverlap(1, 3, 0, 3)
+		solverOutput := config_solver.SolveSize_Overlap(2, 9, 1, 3, "PARTIAL OVERLAP")
+		fmt.Println(solverOutput)
+	},
+}
